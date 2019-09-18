c9ef045cfb3868a5be6daa882f7e3b1601293047
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-18 19:29:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # your code goes here
+
+class Person
+
+  attr_reader :name
+  attr_writer :bank, :happiness, :hygiene
+
+  def initialize(name, bank_account = 25, happiness = 10, hygiene = 10)
+    self
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+end
