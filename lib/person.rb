ce8e8b1c98371bee4eb8448488918e6d9dd40371
github username <github email address>
2019-09-18 17:34:47 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,95 @@
-# your code goes here
+require 'pry'
+
+class Person
+
+  attr_reader :name, :bank_account, :happiness, :hygiene
+  attr_writer :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(number)
+    if number > 10
+      @happiness = 10
+    elsif number < 0
+      @happiness = 0
+    else @happiness = number
+    end
+  end
+
+  def hygiene=(number)
+    if number > 10
+      @hygiene = 10
+    elsif number < 0
+      @hygiene = 0
+    else @hygiene = number
+    end
+  end
+
+  def happy?
+    if @happiness > 7
+      return true
+    else
+      return false
+    end
+  end
+
+  def clean?
+    if @hygiene > 7
+      return true
+    else
+      return false
+    end
+  end
+
+  def get_paid(salary)
+    @bank_account  += salary
+    return "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene=
+    @hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene=
+    @hygiene -= 3
+
+    self.happiness=
+    @happiness += 2
+    return "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness= @happiness + 3
+    friend.happiness= friend.happiness + 3
+    return "Hi #{friend.name}! It's #{@name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    if topic == "politics"
+      self.happiness= @happiness - 2
+      friend.happiness= friend.happiness - 2
+      return "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness= @happiness + 1
+      friend.happiness= friend.happiness + 1
+      return "blah blah sun blah rain"
+    else
+      return "blah blah blah blah blah"
+    end
+  end
+
+end
+
+
+person1 = Person.new("Ivana")
+
+
+#binding.pry
