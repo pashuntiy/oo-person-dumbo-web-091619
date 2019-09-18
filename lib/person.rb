58afb3e74ad1b8ed0c3588af856765c6efca0337
github username <github email address>
2019-09-18 17:52:15 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -32,54 +32,50 @@ class Person
 
   def happy?
     if @happiness > 7
-      return true
+      true
     else
-      return false
+      false
     end
   end
 
   def clean?
     if @hygiene > 7
-      return true
+      true
     else
-      return false
+      false
     end
   end
 
   def get_paid(salary)
     @bank_account  += salary
-    return "all about the benjamins"
+    "all about the benjamins"
   end
 
   def take_bath
-    self.hygiene=
-    @hygiene += 4
-    return "♪ Rub-a-dub just relaxing in the tub ♫"
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
-    self.hygiene=
-    @hygiene -= 3
-
-    self.happiness=
-    @happiness += 2
+    self.hygiene -= 3
+    self.happiness += 2
     return "♪ another one bites the dust ♫"
   end
 
-  def call_friend(friend)
-    self.happiness= @happiness + 3
-    friend.happiness= friend.happiness + 3
-    return "Hi #{friend.name}! It's #{@name}. How are you?"
+  def call_friend(person)
+    self.happiness += 3
+    person.happiness += 3
+    return "Hi #{person.name}! It's #{@name}. How are you?"
   end
 
-  def start_conversation(friend, topic)
+  def start_conversation(person, topic)
     if topic == "politics"
-      self.happiness= @happiness - 2
-      friend.happiness= friend.happiness - 2
+      self.happiness -= 2
+      person.happiness -= 2
       return "blah blah partisan blah lobbyist"
     elsif topic == "weather"
-      self.happiness= @happiness + 1
-      friend.happiness= friend.happiness + 1
+      self.happiness += 1
+      person.happiness += 1
       return "blah blah sun blah rain"
     else
       return "blah blah blah blah blah"
